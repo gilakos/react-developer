17b049f69f20771a3c41b6d46e7d25822ceb4822
Gil Akos <gil@astra.finance>
2017-11-29 10:34:11 -0800

    Feat: Add Delete Button Functionality

--- Core_02/contacts/src/App.js
+++ Core_02/contacts/src/App.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import ListContacts from './ListContacts'
 
 class App extends Component {
   state = {
-    contacts : [
+    contacts: [
       {
         "id": "ryan",
         "name": "Ryan Florence",
         "email": "ryan@reacttraining.com",
         "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
+      }, {
         "id": "michael",
         "name": "Michael Jackson",
         "email": "michael@reacttraining.com",
         "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
+      }, {
         "id": "tyler",
         "name": "Tyler McGinnis",
         "email": "tyler@reacttraining.com",
@@ -24,13 +22,18 @@ class App extends Component {
       }
     ]
   }
-   render() {
-    return (
-      <div>
-        <ListContacts contacts={this.state.contacts}/>
-      </div>
-    )
-   }
- }
+
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter((c) => c.id !== contact.id)
+    }))
+  }
+  
+  render() {
+    return (<div>
+      <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts}/>
+    </div>)
+  }
+}
 
 export default App;
