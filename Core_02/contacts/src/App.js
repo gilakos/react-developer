ed12858fc8a26239083c453d6ab9217fe3573c21
Gil Akos <gil@astra.finance>
2017-11-29 09:41:28 -0800

    Chore: Starter Files Setup

--- Core_02/contacts/src/App.js
+++ Core_02/contacts/src/App.js
@@ -3,19 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello world</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        Hello World
       </div>
-    );
-  }
-}
+    )
+   }
+ }
 
 export default App;
