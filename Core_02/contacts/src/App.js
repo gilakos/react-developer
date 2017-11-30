6efb30bf9bd80470043ad97bd95e2cf5666035bb
Gil Akos <gil@astra.finance>
2017-11-29 16:25:10 -0800

    Feat: Add + Implement React Router

--- Core_02/contacts/src/App.js
+++ Core_02/contacts/src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
+import { Route } from 'react-router-dom'
 import ListContacts from './ListContacts'
 import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
-    screen: 'list', //'create',
     contacts: []
   }
 
@@ -23,10 +23,18 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  createContact(contact) {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat([contact])
+      }))
+    })
+  }
+
   render() {
     return (
-      <div>
-        {this.state.screen === 'list' && (
+      <div className="app">
+        <Route exact path="/" render={() => (
           <ListContacts
             contacts={this.state.contacts}
             onDeleteContact={this.removeContact}
@@ -34,10 +42,15 @@ class App extends Component {
               this.setState({screen:'create'})
             }}
           />
-        )}
-        {this.state.screen === 'create' && (
-          <CreateContact/>
-        )}
+        )}/>
+        <Route path="/create" render={({history})=> (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )}/>
       </div>
     )
   }
